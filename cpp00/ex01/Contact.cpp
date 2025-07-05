9c83725a8445693e54b8da71c4dfa819f6c320bd
Jikuhara <kei2003730@gmail.com>
2025-07-05 19:17:20 +0900

    Privateの使い方わからーん。Publicにするか。

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 19:13:37 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/05 19:14:28 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/05 19:16:53 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 void	add_contact(void)
 {}
 
-void	print_contact(void)
+// え、これPrivateじゃ無理じゃね。。。
+void	print_contact(Contact contact)
 {
-	
+	std::cout << contact.first_name << std::endl;
+	std::cout << contact.last_name << std::endl;
+	std::cout << contact.nickname << std::endl;
+	std::cout << contact.phone_number << std::endl;
+	std::cout << contact.darkest_secret << std::endl;
 }
