98d28fe550dff72b0776edb163f014bf85e784ca
Jikuhara <kei2003730@gmail.com>
2025-07-05 19:54:24 +0900

    うーん、なんか呼び出し方しっくりせえへん。 出力対象がはっきりしてんのに、なんでもっかい仮引数に代入せなあかんねん。

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,47 +6,43 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 19:13:37 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/05 19:48:07 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/05 19:53:39 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-Contact	new_contact(void)
-{
-	Contact contact;
-	std::cout << "Create new contact" << std::endl;
-	while (1)
-	{
-		std::cout << "Please 入力 new contact's first name" << std::endl;
-		std::cin >> contact.first_name;
-		if (!contact.first_name.empty())
-		{
-			break ;
-		}
-		std::cout << "You should 入力 something" << std::endl;
-	}
-	return (contact);
-}
+// Contact	new_contact(void)
+// {
+// 	Contact contact;
+// 	std::cout << "Create new contact" << std::endl;
+// 	while (1)
+// 	{
+// 		std::cout << "Please 入力 new contact's first name" << std::endl;
+// 		std::cin >> contact.first_name;
+// 		if (!contact.first_name.empty())
+// 		{
+// 			break ;
+// 		}
+// 		std::cout << "You should 入力 something" << std::endl;
+// 	}
+// 	return (contact);
+// }
 
-// え、これPrivateじゃ無理じゃね。。。→解決
 void	Contact::print_contact(Contact contact)
 {
-	std::cout << contact.first_name << std::endl;
-	std::cout << contact.last_name << std::endl;
-	std::cout << contact.nickname << std::endl;
-	std::cout << contact.phone_number << std::endl;
-	std::cout << contact.darkest_secret << std::endl;
+	std::cout << "First name: " << contact.first_name << std::endl;
+	std::cout << "Last name: " << contact.last_name << std::endl;
+	std::cout << "Nickname: " << contact.nickname << std::endl;
+	std::cout << "Phone number: " << contact.phone_number << std::endl;
+	std::cout << "Darkest secret: " << contact.darkest_secret << std::endl;
 }
 
 int main(int argc, char const *argv[])
 {
 	Contact contact;
 
-	init_contact(&contact);
-	Contact test_new_contact = new_contact();
-	print_contact(contact);
-	print_contact(test_new_contact);
+	contact.print_contact(contact);
 	return 0;
 }
 
