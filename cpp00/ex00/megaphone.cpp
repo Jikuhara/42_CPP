2e87b6238657fd8dc89d794682f9926fe93e94a8
Jikuhara <kei2003730@gmail.com>
2025-07-05 16:22:08 +0900

    なんやこれーーーー。なんで数字で出てくんねん。

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -18,7 +18,7 @@ int	main(int argc, char const *argv[])
 		std::cout << arg << std::endl;
 		for (size_t j = 0; j < arg.size(); j++)
 		{
-			std::cout << arg[j] - 32;
+			std::cout << std::toupper(arg[j]);
 		}
 		std::cout << std::endl;
 	}
