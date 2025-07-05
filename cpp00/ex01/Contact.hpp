74c427e334757ea0a7871f0522fb1718a7a0c7d7
Jikuhara <kei2003730@gmail.com>
2025-07-05 19:08:20 +0900

    とりあえず、Contactを作成してみた。

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 19:03:54 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/05 19:07:44 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+
+
+class Contact
+{
+	private:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+};
+
+#endif
