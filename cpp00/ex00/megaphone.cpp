f6af920d810da19e8a63883459eb7692cd6bc915
Jikuhara <kei2003730@gmail.com>
2025-07-05 16:34:27 +0900

    >./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
    テスト用に改行とか色々入れてたのに気付いた。
    
    しかもコマンドライン引数ごとに改行いらんなこれ。

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -10,16 +10,14 @@ int	main(int argc, char const *argv[])
 	}
 	if (argc == 1)
 	{
-		std::cout << "test" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	}
 	for (int i = 1; i < argc; i++)
 	{
 		std::string arg = argv[i];
-		std::cout << arg << std::endl;
 		for (size_t j = 0; j < arg.size(); j++)
 		{
 			std::cout << static_cast<unsigned char>(std::toupper(arg[j]));
 		}
-		std::cout << std::endl;
 	}
 }
